e5857f366599fa5a34015b532c5b2a1b7f8bbb35
Mathsterk <mathiassterk@gmail.com>
2019-01-24 00:02:29 +0100

    Update kuk.py

--- kuk.py
+++ kuk.py
@@ -87,9 +87,11 @@ while True:
           led.value(1)
           time.sleep(0.05)
           led.value(0)
-
-
-      time.sleep(1)
+        else {
+          time.sleep(1)
+        }
+        
+      time.sleep(0.5)
     
 
     
